fc2ace750a0ad96080bcfb1ae18e6be4de7bc656
Lu <matias.rephael.luke+github@gmail.com>
2025-10-14 20:16:33 +0800

    try, di ko pa rin gets

--- PagingAndSegmentation/src/Main.java
+++ PagingAndSegmentation/src/Main.java
@@ -1,8 +1,6 @@
 import java.util.Scanner;
 public class Main {
 
-     // <-- normally this goes at the very top (outside class)
-
     // Constants
     static final int PAGE_SIZE = 1024;
 
@@ -30,21 +28,7 @@ public class Main {
         int[] result = new int[1]; // store physical address
 
         try {
-            if (segmentNum < 0 || segmentNum >= segmentTable.length) {
-                throw new Exception("Segmentation Fault: Invalid Segment Number");
-            }
-
-            Segment seg = segmentTable[segmentNum];
-
-            if (pageNum >= seg.limit) {
-                throw new Exception("Segmentation Fault: Page exceeds segment limit");
-            }
-
-            if (offset >= PAGE_SIZE) {
-                throw new Exception("Offset exceeds page size");
-            }
-
-            int frameNum = seg.pageTable[pageNum];
+            int frameNum = getFrameNum(segmentNum, pageNum, offset);
             int physicalAddress = frameNum * PAGE_SIZE + offset;
             result[0] = physicalAddress;
 
@@ -57,8 +41,26 @@ public class Main {
         return result;
     }
 
+    private static int getFrameNum(int segmentNum, int pageNum, int offset) throws Exception {
+        if (segmentNum < 0 || segmentNum >= segmentTable.length)
+            throw new Exception("Segmentation Fault: Invalid Segment Number");
+
+        Segment seg = segmentTable[segmentNum];
+
+        if (pageNum >= seg.limit) {
+            throw new Exception("Segmentation Fault: Page exceeds segment limit");
+        }
+
+        if (offset >= PAGE_SIZE) {
+            throw new Exception("Offset exceeds page size");
+        }
+
+        int frameNum = seg.pageTable[pageNum];
+        return frameNum;
+    }
+
     public static void main(String[] args) {
-        System.out.println("hello os");
+        System.out.println("Memory Management System with Paging and Segmentation");
 
         Main mmu = new Main();
         Scanner sc = new Scanner(System.in);
